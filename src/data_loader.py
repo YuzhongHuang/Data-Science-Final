4cf7a9ef436faa0dd965f11eef2ba2f9fd00c0d7
YuzhongHuang <yuzhong.huang@students.olin.edu>
2016-04-19 12:27:03 -0400

    create the 27 features; named as features.pickle; in the form of(features, target), here the targets is blood

--- src/data_loader.py
+++ src/data_loader.py
@@ -16,6 +16,62 @@ import pickle
 from theano import *
 import theano.tensor as T
 
+def read_csv(dataset):
+    """ Reads an csv file and converts to (train, val, test)
+    with each of the form (inputs, targets)
+
+    :type dataset: string
+    :param dataset: the path to the dataset
+
+    """
+    # default values
+    target_name = "blood" # default target column
+    train_percent = 0.5 # default trainset percentage
+    val_percent = 0.2 # default valset percentage
+    test_percent = 0.3 # default testset percentage
+
+    # process dataset to form (inputs, targets)
+    data = pandas.read_csv(dataset) # import from dataset
+    targets = data[target_name].as_matrix() # get the target column as numpy array
+
+    data = data.drop(target_name, 1) # get a dataframe with the remaining features
+    inputs = data.as_matrix() # get the feature matrix as numpy matrix
+    inputs = inputs[:, 1:] # drop the index column
+
+    entries = targets.shape[0] # total entries of dataset
+
+    # use permutation to generate random indices
+    indices = numpy.random.permutation(entries) 
+
+    # get randomized indices for each datasets
+    train_idx = indices[:int(train_percent*entries)] 
+    val_idx = indices[int(train_percent*entries):int(test_percent*entries)]
+    test_idx = indices[int(test_percent*entries):]
+
+    train = (inputs[train_idx,:], targets[train_idx])
+    val = (inputs[val_idx,:], targets[val_idx])
+    test = (inputs[test_idx,:], targets[test_idx])
+
+    return train, val, test
+
+def shared_dataset(data_xy, borrow=True):
+    """ Function that loads the dataset into shared variables
+
+    The reason we store our dataset in shared variables is to allow
+    Theano to copy it into the GPU memory (when code is run on GPU).
+    Since copying data into the GPU is slow, copying a minibatch everytime
+    is needed (the default behaviour if the data is not in a shared
+    variable) would lead to a large decrease in performance.
+    """
+    data_x, data_y = data_xy
+    shared_x = theano.shared(numpy.asarray(data_x,
+                                           dtype=theano.config.floatX),
+                             borrow=borrow)
+    shared_y = theano.shared(numpy.asarray(data_y,
+                                           dtype=theano.config.floatX),
+                             borrow=borrow)
+    return shared_x, T.cast(shared_y, 'int32')
+
 def load_data(dataset):
     """ Loads the dataset
 
@@ -23,70 +79,6 @@ def load_data(dataset):
     :param dataset: the path to the dataset
     
     """
-
-    def read_csv(dataset):
-        """ Reads an csv file and converts to (train, val, test)
-        with each of the form (inputs, targets)
-
-        :type dataset: string
-        :param dataset: the path to the dataset
-
-        """
-        # default values
-        target_name = "blood" # default target column
-        train_percent = 0.5 # default trainset percentage
-        val_percent = 0.2 # default valset percentage
-        test_percent = 0.3 # default testset percentage
-
-        # process dataset to form (inputs, targets)
-        data = pandas.read_csv(dataset) # import from dataset
-        targets = data[target_name].as_matrix() # get the target column as numpy array
-
-        data = data.drop(target_name, 1) # get a dataframe with the remaining features
-        inputs = data.as_matrix() # get the feature matrix as numpy matrix
-        inputs = inputs[:, 1:] # drop the index column
-
-        entries = targets.shape[0] # total entries of dataset
-
-        # use permutation to generate random indices
-        indices = numpy.random.permutation(entries) 
-
-        # get randomized indices for each datasets
-        train_idx = indices[:int(train_percent*entries)] 
-        val_idx = indices[int(train_percent*entries):int(test_percent*entries)]
-        test_idx = indices[int(test_percent*entries):]
-
-        train = (inputs[train_idx,:], targets[train_idx])
-        val = (inputs[val_idx,:], targets[val_idx])
-        test = (inputs[test_idx,:], targets[test_idx])
-
-        return train, val, test
-
-    def shared_dataset(data_xy, borrow=True):
-        """ Function that loads the dataset into shared variables
-
-        The reason we store our dataset in shared variables is to allow
-        Theano to copy it into the GPU memory (when code is run on GPU).
-        Since copying data into the GPU is slow, copying a minibatch everytime
-        is needed (the default behaviour if the data is not in a shared
-        variable) would lead to a large decrease in performance.
-        """
-        data_x, data_y = data_xy
-        shared_x = theano.shared(numpy.asarray(data_x,
-                                               dtype=theano.config.floatX),
-                                 borrow=borrow)
-        shared_y = theano.shared(numpy.asarray(data_y,
-                                               dtype=theano.config.floatX),
-                                 borrow=borrow)
-        # When storing data on the GPU it has to be stored as floats
-        # therefore we will store the labels as ``floatX`` as well
-        # (``shared_y`` does exactly that). But during our computations
-        # we need them as ints (we use labels as index, and if they are
-        # floats it doesn't make sense) therefore instead of returning
-        # ``shared_y`` we will have to cast it to int. This little hack
-        # lets ous get around this issue
-        return shared_x, T.cast(shared_y, 'int32')
-
     train_set, valid_set, test_set = read_csv(dataset)
 
     test_set_x, test_set_y = shared_dataset(test_set)
--- src/output.py
+++ src/output.py
@@ -11,24 +11,29 @@ import pickle
 
 from dA import dA
 from data_loader import load_data
+from data_loader import shared_dataset
 
 
 def get_features(da, inputs):
-	features = []
-	for inp in inputs:
-		feature = da.get_reconstructed_input(da.get_hidden_values(inp)).eval()
-		features.append(feature)
-	return features
+    features = []
+    for inp in inputs:
+        feature = da.get_reconstructed_input(da.get_hidden_values(inp)).eval()
+        features.append(feature)
+    return numpy.asarray(features)
 
-def output(da_name, inputs, save_name):
-	da = dA.load(da_name)
-	features = get_features(da, inputs[0:2])
+def output(da_name, data_x, data_y, save_name):
+    da = dA.load(da_name)
+    features = get_features(da, data_x)
+    new_data = (features, data_y)
+    shared_new_data = shared_dataset(new_data)
 
-	with open(save_name+'.pickle', 'wb') as handle:
-        pickle.dump(features, handle)
-
+    with open(save_name+'.pickle', 'wb') as handle:
+        pickle.dump(shared_new_data, handle)
 
 datasets = load_data("../data/processed/clean_synthesized_data.csv")
 train_set_x, train_set_y = datasets[0]
 
-output("model", train_set_x.get_value(borrow=True))
+train_set_x = train_set_x.get_value(borrow=True)
+train_set_y = train_set_y.eval()
+
+output("model", train_set_x, train_set_y, "features")
